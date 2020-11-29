482dcd0ac06836be52ce471d26ed80461a081448
Brijesh Rohilla <iambrijeshrohilla@gmail.com>
2020-11-29 17:15:05 +0530

    local-share frist commit

---
+++ app.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'public/FILES')));
+
+// Create path to shared folder
+const filePath = path.join(__dirname, 'public/FILES');
+
+app.get('/', (_req, res) => {
+  const arr = [];
+
+  fs.readdir(filePath, (err, files) => {
+    if (err) {
+      console.log(`Unable to scan directory: ${err}`);
+    }
+
+    // create array of all files found in directory
+    files.forEach((file) => {
+      arr.push(file);
+    });
+
+    res.set('Cache-Control', 'public, no-store');
+    res.render('index', { doc: arr });
+  });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.end();
+});
+
+module.exports = app;
